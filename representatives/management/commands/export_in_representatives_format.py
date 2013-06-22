b07a3bc3892fe68a68c97d3dcdf63426db3270c3
Laurent Peuch <cortex@worlddomination.be>
2013-06-22 15:39:37 +0200

    [enh] export personal data of a representative

--- representatives/management/commands/export_in_representatives_format.py
+++ representatives/management/commands/export_in_representatives_format.py
@@ -8,8 +8,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         result = []
+        personal_fields = ("first_name", "last_name", "full_name", "birth_place", "cv")
+        gender_dict = dict(Representative.GENDER)
         for representative in Representative.objects.all():
             reps = {"id": representative.remote_id}
+            reps["personal"] = {field: getattr(representative, field) for field in personal_fields}
+            reps["personal"]["gender"] = gender_dict[representative.gender]
+            reps["personal"]["birth_date"] = representative.birth_date.strftime("%F") if representative.birth_date else None
 
             result.append(reps)
 
