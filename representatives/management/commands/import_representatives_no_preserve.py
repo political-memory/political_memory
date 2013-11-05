8a6f3175d2849c5ee181c81144bcc0dfb91fab9b
Laurent Peuch <cortex@worlddomination.be>
2013-11-05 21:21:43 +0100

    [mod] remove useless stuff

--- representatives/management/commands/import_representatives_no_preserve.py
+++ representatives/management/commands/import_representatives_no_preserve.py
@@ -4,8 +4,5 @@ from django.core.management.base import BaseCommand
 from representatives.utils import import_representatives_from_format
 
 class Command(BaseCommand):
-    args = '<poll_id poll_id ...>'
-    help = 'Closes the specified poll for voting'
-
     def handle(self, *args, **options):
         import_representatives_from_format(json.load(sys.stdin))
