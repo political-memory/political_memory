1850d5e9b3b604166343b6b2200eb61a76159956
luxcem <af@laquadrature.net>
2015-03-03 16:52:13 +0100

    adds photo to representative model

--- representatives/models.py
+++ representatives/models.py
@@ -33,6 +33,7 @@ class Representative(models.Model):
     birth_place = models.CharField(max_length=255, blank=True, null=True)
     birth_date = models.DateField(blank=True, null=True)
     cv = models.TextField(blank=True, null=True)
+    photo = models.CharField(max_length=512, null=True)
 
 
 class Contact(models.Model):
