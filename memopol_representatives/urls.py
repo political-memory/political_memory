39e1538fc5e14c0299ceef58fdbc4792bbe76a80
luxcem <af@laquadrature.net>
2015-03-13 17:07:22 +0100

    Adds a view for representatives details and a (embryonic) search

--- memopol_representatives/urls.py
+++ memopol_representatives/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 
 from memopol_representatives import views
 
-urlpatterns = patterns('',
-    url(r'^$', views.IndexView.as_view(), name='index'),
+urlpatterns = patterns(
+    '',
+    url(r'^$', views.index, name='index'),
+    url(r'^view/(?P<num>\d+)$', views.view, name='view'),
 )
--- memopol_representatives/views.py
+++ memopol_representatives/views.py
@@ -1,11 +1,47 @@
-# from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
 
 from representatives.models import Representative
 
 
-class IndexView(generic.ListView):
-    template_name = 'memopol_representatives/list.html'
+def index(request):
+    context = {}
+    if request.GET.get('search'):
+        search = request.GET.get('search')
+        representative_list = Representative.objects.filter(
+            Q(full_name__icontains=search)
+        )
+        queries_without_page = request.GET.copy()
+        if 'page' in queries_without_page:
+            del queries_without_page['page']
+        context['queries'] = queries_without_page
+    else:
+        representative_list = Representative.objects.all()
 
-    def get_queryset(self):
-        return Representative.objects.all()
+    paginator = Paginator(representative_list, 5)
+
+    page = request.GET.get('page')
+    try:
+        representatives = paginator.page(page)
+    except PageNotAnInteger:
+        representatives = paginator.page(1)
+    except EmptyPage:
+        representatives = paginator.page(paginator.num_pages)
+
+    context['representatives'] = representatives
+    return render(
+        request,
+        'memopol_representatives/list.html',
+        context
+    )
+
+
+def view(request, num):
+    representative = get_object_or_404(Representative, pk=num)
+
+    return render(
+        request,
+        'memopol_representatives/view.html',
+        {'representative': representative}
+    )
