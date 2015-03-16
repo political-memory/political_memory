f0c7c3b40405edfa60f7f3c5b560e1de2a10d4cf
luxcem <af@laquadrature.net>
2015-03-16 16:32:55 +0100

    Adds an option for quiet import

--- representatives/management/commands/update_eu_mps.py
+++ representatives/management/commands/update_eu_mps.py
@@ -9,11 +9,17 @@ from representatives.utils import import_representatives_from_format
 
 
 class Command(BaseCommand):
+
     def handle(self, *args, **options):
+        if args and args[0] == 'q':
+            verbose = False
+        else:
+            verbose = True
+
         compotista_server = getattr(settings,
                                     'REPRESENTATIVES_COMPOTISTA_SERVER',
                                     'http://compotista.mm.staz.be')
         url = compotista_server + "/latest/"
         import_representatives_from_format(
             json.load(urllib2.urlopen(url)),
-            verbose=True)
+            verbose=verbose)
