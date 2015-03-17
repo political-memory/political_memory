f2c45ef90d529a61c83a60edea046a426f8c9d85
luxcem <af@laquadrature.net>
2015-03-17 15:29:15 +0100

    Adds atomic on a command

--- representatives/management/commands/fix_mandates_active.py
+++ representatives/management/commands/fix_mandates_active.py
@@ -1,10 +1,11 @@
 import datetime
 from django.core.management.base import BaseCommand
 from representatives.models import Mandate
-
+from django.db import transaction
 
 class Command(BaseCommand):
 
+    @transaction.atomic
     def handle(self, *args, **options):
         date = datetime.datetime.now().date()
         for mandate in Mandate.objects.all():
