71e274b057bfeaf6dc71c2d44f419aac89d25b16
luxcem <af@laquadrature.net>
2015-06-01 14:21:46 +0200

    merge migrations

---
+++ representatives/migrations/0014_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('representatives', '0011_auto_20150529_1516'),
+        ('representatives', '0013_auto_20150528_1617'),
+    ]
+
+    operations = [
+    ]
