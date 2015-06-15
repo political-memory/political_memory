36b2b07154c6d7ef14f73cbb1b7da78e73719e32
Arnaud Fabre <arnaud.fabre@camobscura.fr>
2015-06-15 14:58:29 +0200

    templates

--- memopol/urls.py
+++ memopol/urls.py
@@ -1,12 +1,37 @@
+# coding: utf-8
+
+# This file is part of memopol.
+#
+# memopol is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or any later version.
+#
+# memopol is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU General Affero Public
+# License along with django-representatives.
+# If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2015 Arnaud Fabre <af@laquadrature.net>
+
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from adminplus.sites import AdminSitePlus
 
 import core.views
+
+admin.site = AdminSitePlus()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'memopol.views.home', name='home'),
     url(r'^$', core.views.HomeView.as_view(), name='index'),
     url('', include('legislature.urls', namespace='legislature')),
+    url('', include('votes.urls', namespace='votes')),
     url(r'^admin/', include(admin.site.urls)),
 )
