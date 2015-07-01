ac077903855dfddb180e2a1ec41227a8f1f57807
Arnaud Fabre <arnaud.fabre@camobscura.fr>
2015-07-01 12:24:30 +0200

    celery settings

--- memopol/celery.py
+++ memopol/celery.py
@@ -20,6 +20,27 @@
 
 from __future__ import absolute_import
 
-# This will make sure the app is always imported when
-# Django starts so that shared_task will use this app.
-from .celery import app as celery_app
+import os
+
+from celery import Celery
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'memopol.settings')
+
+from django.conf import settings
+
+app = Celery(
+    'memopol',
+    broker='redis://localhost',
+    backend='redis://localhost'
+)
+
+# Using a string here means the worker will not have to
+# pickle the object when using Windows.
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
