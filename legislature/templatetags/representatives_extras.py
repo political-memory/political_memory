e571ce4cd9f7df64aafa90e5ec296a7cccfdc96d
Arnaud Fabre <arnaud.fabre@resel.fr>
2015-07-23 14:10:25 +0200

    Improvement of representatives pages

--- legislature/templatetags/representatives_extras.py
+++ legislature/templatetags/representatives_extras.py
@@ -1,10 +1,43 @@
+# coding: utf-8
+
+# This file is part of memopol.
+#
+# memopol is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or any later version.
+#
+# memopol is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU General Affero Public
+# License along with django-representatives.
+# If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2015 Arnaud Fabre <af@laquadrature.net>
+
+"""
+This file contains all templatetags used by the representative app
+"""
+
 from django import template
 from django.core.urlresolvers import reverse
 from django.utils.safestring import mark_safe
+from django.contrib.humanize.templatetags.humanize import naturalday
 
 from representatives.models import Mandate, Group
 
 register = template.Library()
+
+@register.filter
+def mandate_date(date, arg=None):
+    if date.year == 9999:
+        return 'present'
+    else:
+        return naturalday(date, arg)
+
 
 @register.filter
 def position_icon(position):
@@ -43,9 +76,9 @@ def score_label(score):
 
 @register.filter
 def country_flag(country):
-    return mark_safe('{} <span class="flag-icon flag-icon-{}"></span>'.format(
-        country.name,
-        country.code.lower()
+    return mark_safe('<span class="flag-icon flag-icon-{code}"></span> {name}'.format(
+        name=country.name,
+        code=country.code.lower()
     ))
 
 
