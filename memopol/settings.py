b31039394a378ca5a7f1d0130918fa0941f5d118
jpic <jamespic@gmail.com>
2015-10-30 15:27:36 +0100

    Added a sample config.json for local development.

--- memopol/settings.py
+++ memopol/settings.py
@@ -121,7 +121,9 @@ DATABASES = {
     }
 }
 
-if get_param('database_server') == 'mysql':
+if get_param('local'):
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+elif get_param('database_server') == 'mysql':
     DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
 elif get_param('database_server') == 'postgresql':
     DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
