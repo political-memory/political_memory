08fbac1d0c6f6b4053959bf569ff92ac931366d5
jpic <jamespic@gmail.com>
2015-12-16 20:57:33 +0100

    Bugfix: was passing a string num_by_page

--- core/utils.py
+++ core/utils.py
@@ -26,7 +26,8 @@ def render_paginate_list(request, object_list, template_name):
     Render a paginated list of representatives
     """
     pagination_limits = (10, 20, 50, 100)
-    num_by_page = request.GET.get('limit', 30)
+    num_by_page = request.GET.get('limit', '30')
+    num_by_page = int(num_by_page) if num_by_page.isdigit() else 1
     paginator = Paginator(object_list, num_by_page)
     number = request.GET.get('page', '1')
     number = int(number) if number.isdigit() else 1
