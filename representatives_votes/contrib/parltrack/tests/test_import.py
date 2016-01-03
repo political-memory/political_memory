d520f06d7d9159b6312e8051113fde1a1f023950
Nicolas Joyard <joyard.nicolas@gmail.com>
2016-01-03 23:43:49 +0100

    Add single dossier sync from parltrack

--- representatives_votes/contrib/parltrack/tests/test_import.py
+++ representatives_votes/contrib/parltrack/tests/test_import.py
@@ -1,9 +1,11 @@
-import pytest
-import os
 import copy
+import mock
+import os
+import pytest
 
 from django.core.serializers.json import Deserializer
 from django.core.management import call_command
+from django.test import TestCase
 from representatives_votes.contrib.parltrack import import_dossiers
 from representatives_votes.contrib.parltrack import import_votes
 from representatives_votes.models import Dossier, Proposal, Vote
@@ -50,3 +52,42 @@ def test_parltrack_import_votes():
         'dossiers_expected.json'))
 
     _test_import('votes', import_votes.main)
+
+
+class DossierTest(TestCase):
+    def setUp(self):
+        for model in (Representative, Dossier, Proposal, Vote):
+            model.objects.all().delete()
+
+    def test_parltrack_import_single_dossier(self):
+        call_command('loaddata', os.path.join(os.path.abspath(
+            representatives.__path__[0]), 'fixtures',
+            'representatives_test.json'))
+
+        with self.assertNumQueries(16):
+            _test_import('single', import_dossiers.import_single)
+
+    def test_parltrack_sync_dossier(self):
+        call_command('loaddata', os.path.join(os.path.abspath(
+            representatives.__path__[0]), 'fixtures',
+            'representatives_test.json'))
+        call_command('loaddata', os.path.join(os.path.dirname(__file__),
+            'sync_fixture.json'))
+
+        reference = '2012/2002(INI)'
+        mock_file = os.path.join(os.path.dirname(__file__),
+            'parltrack_dossier_%s.json' % reference.replace('/', '-'))
+        expected_url = '%s/dossier/%s?format=json' % (import_dossiers.BASEURL,
+            reference)
+
+        def callback(stream):
+            import_dossiers.sync_dossier(reference)
+
+        with mock.patch('urllib2.urlopen') as urlopen:
+            with open(mock_file, 'r') as mock_stream:
+                urlopen.return_value = mock_stream
+
+                with self.assertNumQueries(3):
+                    _test_import('sync', callback)
+
+            urlopen.assert_called_with(expected_url)
