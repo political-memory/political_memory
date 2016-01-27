842da8b404a9b337ce44eeb4804616c03625748b
jpic <jamespic@gmail.com>
2016-01-27 01:45:09 +0100

    Removed copyright and license from script

--- representatives_votes/serializers.py
+++ representatives_votes/serializers.py
@@ -1,23 +1,5 @@
 # coding: utf-8
 
-# This file is part of toutatis.
-#
-# toutatis is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of
-# the License, or any later version.
-#
-# toutatis is distributed in the hope that it will
-# be useful, but WITHOUT ANY WARRANTY; without even the implied
-# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU General Affero Public
-# License along with django-representatives.
-# If not, see <http://www.gnu.org/licenses/>.
-#
-# Copyright (C) 2015 Arnaud Fabre <af@laquadrature.net>
-
 import representatives_votes.models as models
 from representatives.models import Representative
 from rest_framework import serializers
