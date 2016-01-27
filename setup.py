8587f24cbc6ad264e05441409ccb7b1ec41e3213
jpic <jamespic@gmail.com>
2016-01-27 04:23:10 +0100

    Bump 0.0.14

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-representatives',
-    version='0.0.13',
+    version='0.0.14',
     description='Base app for government representative',
     author='Laurent Peuch, Olivier Le Thanh Duong, Yohan Boniface, Arnaud Fabre, James Pic',
     author_email='webmaster@memopol.org',
