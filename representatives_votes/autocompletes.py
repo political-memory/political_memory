546c0b971f98ca5e9a75ab0816b2928adbd4ca23
jpic <jamespic@gmail.com>
2016-02-11 21:09:40 +0100

    Added dossier autocomplete view

--- representatives_votes/autocompletes.py
+++ representatives_votes/autocompletes.py
@@ -2,7 +2,20 @@ from dal import autocomplete
 
 from django.db.models import Q
 
-from models import Proposal
+from models import Dossier, Proposal
+
+
+class DossierAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        qs = Dossier.objects.all()
+
+        if self.q:
+            qs = qs.filter(
+                Q(title__icontains=self.q) |
+                Q(reference__icontains=self.q)
+            )
+
+        return qs
 
 
 class ProposalAutocomplete(autocomplete.Select2QuerySetView):
