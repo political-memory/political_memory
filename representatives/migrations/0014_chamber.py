ee751adfec9e407c013053c028aa259df34d413e
Nicolas Joyard <joyard.nicolas@gmail.com>
2016-02-21 16:11:25 +0100

    Fix incorrect chamber migration

--- representatives/migrations/0014_chamber.py
+++ representatives/migrations/0014_chamber.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('fingerprint', models.CharField(unique=True, max_length=40)),
                 ('name', models.CharField(max_length=255)),
-                ('country', models.ForeignKey(related_name='chambers', blank=True, to='representatives.Country', null=True)),
+                ('country', models.ForeignKey(related_name='chambers', to='representatives.Country', null=True)),
             ],
             options={
                 'abstract': False,
