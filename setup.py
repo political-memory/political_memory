cb181b188958565740c8fc62f3706b340a21eeb8
jpic <jamespic@gmail.com>
2016-02-28 22:14:12 +0100

    Release 0.0.16

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-representatives',
-    version='0.0.15',
+    version='0.0.16',
     description='Base app for government representative',
     author='Laurent Peuch, Olivier Le Thanh Duong, Yohan Boniface, Arnaud Fabre, James Pic',
     author_email='webmaster@memopol.org',
