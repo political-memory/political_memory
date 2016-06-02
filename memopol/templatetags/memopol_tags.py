e871ddf51db73fca4c54c85cb11a745bc7310cf2
Nicolas Joyard <joyard.nicolas@gmail.com>
2016-06-02 08:40:31 +0200

    Fix social nickname extraction

--- memopol/templatetags/memopol_tags.py
+++ memopol/templatetags/memopol_tags.py
@@ -10,23 +10,23 @@ link = '<a class="{network}-link" href="{url}" target="_blank">{label}</a>'
 @register.filter
 def twitter_link(url):
     return mark_safe(link.format(network='twitter', url=url,
-                                 label=re.sub(r'.*/@?([^/]+)', '@\\1', url)))
+        label=re.sub(r'.*/@?([^/]+)', '@\\1', re.sub(r'/$', '', url.strip()))))
 
 
 @register.filter
 def facebook_link(url):
     return mark_safe(link.format(network='facebook', url=url,
-                                 label=re.sub(r'.*/([^/]+)', '\\1', url)))
+        label=re.sub(r'.*/([^/]+)', '\\1', re.sub(r'/$', '', url.strip()))))
 
 
 @register.filter
 def website_link(url):
     short_url = re.sub(r'^https?://([^/]+).*', '\\1', url)
     return mark_safe(link.format(network='website', url=url,
-                                 label=short_url))
+        label=short_url))
 
 
 @register.filter
 def email_link(address):
     return mark_safe(link.format(network='email', url='mailto:%s' % address,
-                                 label=address))
+        label=address))
