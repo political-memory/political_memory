a79996b72aafb89a4a4d278c86c94a9cf01847b8
Nicolas Joyard <joyard.nicolas@gmail.com>
2016-06-02 07:13:49 +0200

    Add other websites

--- memopol/templatetags/memopol_tags.py
+++ memopol/templatetags/memopol_tags.py
@@ -4,17 +4,23 @@ from django.utils.safestring import mark_safe
 import re
 
 register = template.Library()
-link = '<a class="{network}-link" href="{url}" target="_blank">{user}</a>'
+link = '<a class="{network}-link" href="{url}" target="_blank">{label}</a>'
 
 
 @register.filter
 def twitter_link(url):
     return mark_safe(link.format(network='twitter', url=url,
-                                 user=re.sub(r'.*/@?([^/]+)', '@\\1', url)))
+                                 label=re.sub(r'.*/@?([^/]+)', '@\\1', url)))
 
 
 @register.filter
 def facebook_link(url):
     return mark_safe(link.format(network='facebook', url=url,
-                                 user=re.sub(r'.*/([^/]+)', '\\1', url)))
+                                 label=re.sub(r'.*/([^/]+)', '\\1', url)))
 
+
+@register.filter
+def website_link(url):
+    short_url = re.sub(r'^https?://([^/]+).*', '\\1', url)
+    return mark_safe(link.format(network='website', url=url,
+                                 label=short_url))
