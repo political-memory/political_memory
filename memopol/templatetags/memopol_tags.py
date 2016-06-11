7343014fb3ac94c210b8af76eafcf73b019c2d6e
Nicolas Joyard <joyard.nicolas@gmail.com>
2016-06-11 10:46:19 +0200

    Import templatetags from django-representatives[-votes]

--- memopol/templatetags/memopol_tags.py
+++ memopol/templatetags/memopol_tags.py
@@ -21,6 +21,10 @@ def fix_url(url):
     return re.sub('^(https?://)?', 'https://', url.strip())
 
 
+def cssify(string):
+    return re.sub('[^a-z_-]', '', string.lower())
+
+
 @register.filter
 def twitter_link(url):
     furl = fix_url(url)
