087679f3ff9ed2c0d5e89037d753ae8d6563d1da
Nicolas Joyard <joyard.nicolas@gmail.com>
2016-07-04 18:47:29 +0200

    Add search to dossiers

--- memopol/tests/test_dossiers.py
+++ memopol/tests/test_dossiers.py
@@ -20,6 +20,26 @@ class DossiersTest(ResponseDiffMixin, TestCase):
         # 1 query for recommendations
         self.responsediff_test('/votes/dossier/', 4)
 
+    def test_dossier_search(self):
+        # session setup
+        self.client.get('/votes/dossier/')
+
+        # 1 query for dossier count
+        # 1 query for dossiers
+        # 1 query for proposals
+        # 1 query for recommendations
+        q = 'acta'
+        self.responsediff_test('/votes/dossier/?search=%s' % q, 4)
+
+    def test_dossier_search_noresults(self):
+        # session setup
+        self.client.get('/votes/dossier/')
+
+        # 1 query for dossier count
+        # nothing else since count = 0
+        q = 'no-dossier-will-have-that-title-ever'
+        self.responsediff_test('/votes/dossier/?search=%s' % q, 1)
+
     def test_dossier_detail(self):
         # Get 1st dossier in dataset
         dossier = Dossier.objects.order_by('pk')[0]
