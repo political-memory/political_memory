5fbcc1ac903169fe8d934e3f82af6bfd3884a6d2
Nicolas Joyard <joyard.nicolas@gmail.com>
2016-07-20 08:05:16 +0200

    Add sorting tests

--- memopol/tests/test_dossiers.py
+++ memopol/tests/test_dossiers.py
@@ -47,6 +47,19 @@ class DossiersTest(ResponseDiffMixin, TestCase):
         q = 'no-dossier-will-have-that-title-ever'
         self.responsediff_test('/votes/dossier/?search=%s' % q, 2)
 
+    def test_dossier_sorting(self):
+        # session setup
+        self.client.get('/votes/dossier/?sort_by=title&sort_dir=desc')
+
+        # 1 query for chamber list (search form)
+        # 1 query for dossier count
+        # 1 query for dossiers
+        # 1 query for proposals
+        # 1 query for recommendations
+        # 1 query for documents
+        # 1 query for document chambers
+        self.responsediff_test('/votes/dossier/', 7)
+
     def test_dossier_detail(self):
         # Get 1st dossier in dataset
         dossier = Dossier.objects.order_by('pk')[0]
