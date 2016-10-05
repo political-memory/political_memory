82a3b1dcb9887d92b55b82efa4326a1653f7630c
Nicolas Joyard <joyard.nicolas@gmail.com>
2016-10-05 10:56:18 +0200

    Fix rep import tests (using natural keys)

--- src/representatives/models.py
+++ src/representatives/models.py
@@ -20,18 +20,35 @@ class TimeStampedModel(models.Model):
         abstract = True
 
 
+class CountryManager(models.Manager):
+    def get_by_natural_key(self, code):
+        return self.get(code=code)
+
+
 class Country(models.Model):
+    objects = CountryManager()
+
     name = models.CharField(max_length=255)
     code = models.CharField(max_length=2, unique=True)
 
     def __unicode__(self):
         return u'{} [{}]'.format(self.name, self.code)
 
+    def natural_key(self):
+        return (self.code,)
+
+
+class RepresentativeManager(models.Manager):
+    def get_by_natural_key(self, slug):
+        return self.get(slug=slug)
+
 
 class Representative(TimeStampedModel):
     """
     Base model for representatives
     """
+
+    objects = RepresentativeManager()
 
     slug = models.SlugField(max_length=100, unique=True)
     first_name = models.CharField(max_length=255, blank=True, default='')
@@ -51,6 +68,9 @@ class Representative(TimeStampedModel):
 
     def __unicode__(self):
         return smart_unicode(self.full_name)
+
+    def natural_key(self):
+        return (self.slug,)
 
     def gender_as_str(self):
         genders = {0: 'N/A', 1: 'F', 2: 'M'}
@@ -79,7 +99,15 @@ class WebSite(Contact):
     kind = models.CharField(max_length=255, blank=True, default='')
 
 
+class AddressManager(models.Manager):
+    def get_by_natural_key(self, name, kind, representative_slug):
+        representative = Representative.objects.get(slug=representative_slug)
+        return self.get(name=name, kind=kind, representative=representative)
+
+
 class Address(Contact):
+    objects = AddressManager()
+
     country = models.ForeignKey(Country)
     city = models.CharField(max_length=255, blank=True, default='')
     street = models.CharField(max_length=255, blank=True, default='')
@@ -91,6 +119,10 @@ class Address(Contact):
     name = models.CharField(max_length=255, blank=True, default='')
     location = models.CharField(max_length=255, blank=True, default='')
 
+    def natural_key(self):
+        representative_nk = self.representative.natural_key()
+        return (self.name, self.kind) + representative_nk
+
 
 class Phone(Contact):
     number = models.CharField(max_length=255, blank=True, default='')
@@ -98,10 +130,17 @@ class Phone(Contact):
     address = models.ForeignKey(Address, null=True, related_name='phones')
 
 
+class ChamberManager(models.Manager):
+    def get_by_natural_key(self, abbreviation):
+        return self.get(abbreviation=abbreviation)
+
+
 class Chamber(models.Model):
     """
     A representative chamber
     """
+    objects = ChamberManager()
+
     name = models.CharField(max_length=255)
     country = models.ForeignKey('Country', null=True, related_name='chambers')
     abbreviation = models.CharField(max_length=10, blank=True, default='',
@@ -110,11 +149,26 @@ class Chamber(models.Model):
     def __unicode__(self):
         return u'{} [{}]'.format(self.name, self.abbreviation)
 
+    def natural_key(self):
+        return (self.abbreviation,)
+
+
+class GroupManager(models.Manager):
+    def get_by_natural_key(self, name, kind, chamber_nk):
+        if chamber_nk:
+            chamber = Chamber.objects.get_by_natural_key(chamber_nk)
+        else:
+            chamber = None
+
+        return self.get(name=name, kind=kind, chamber=chamber)
+
 
 class Group(TimeStampedModel):
     """
     An entity represented by a representative through a mandate
     """
+    objects = GroupManager()
+
     name = models.CharField(max_length=511, db_index=True)
     abbreviation = models.CharField(max_length=10, blank=True, default='',
         db_index=True)
@@ -128,14 +182,30 @@ class Group(TimeStampedModel):
     def __unicode__(self):
         return unicode(self.name)
 
+    def natural_key(self):
+        chamber_nk = self.chamber.natural_key() if self.chamber else (None,)
+        return (self.name, self.kind) + chamber_nk
+
     class Meta:
         ordering = ('name',)
+
+
+class ConstituencyManager(models.Manager):
+    def get_by_natural_key(self, name, country_nk):
+        if country_nk:
+            country = Country.objects.get_by_natural_key(country_nk)
+        else:
+            country = None
+
+        return self.get(name=name, country=country)
 
 
 class Constituency(TimeStampedModel):
     """
     An authority for which a representative has a mandate
     """
+    objects = ConstituencyManager()
+
     name = models.CharField(max_length=255)
     country = models.ForeignKey('Country', null=True, blank=True,
         related_name='constituencies')
@@ -146,6 +216,10 @@ class Constituency(TimeStampedModel):
 
     def __unicode__(self):
         return unicode(self.name)
+
+    def natural_key(self):
+        country_nk = self.country.natural_key() if self.country else (None,)
+        return (self.name,) + country_nk
 
 
 class MandateManager(models.Manager):
@@ -158,8 +232,18 @@ class MandateManager(models.Manager):
             'constituency')
 
 
-class Mandate(TimeStampedModel):
+class MandateManager(models.Manager):
+    def get_by_natural_key(self, begin_date, end_date, representative_slug,
+                           group_name, group_kind, group_chamber_nk):
+        representative = Representative.objects.get_by_natural_key(
+            representative_slug)
+        group = Group.objects.get_by_natural_key(group_name, group_kind,
+                                                 group_chamber_nk)
+        return self.get(begin_date=begin_date, end_date=end_date,
+                        representative=representative, group=group)
 
+
+class Mandate(TimeStampedModel):
     objects = MandateManager()
 
     group = models.ForeignKey(Group, null=True, related_name='mandates')
@@ -189,6 +273,14 @@ class Mandate(TimeStampedModel):
                     self.role) if self.role else u''),
             constituency=self.constituency,
             group=self.group)
+
+    def natural_key(self):
+        rep_nk = self.representative.natural_key() \
+            if self.representative else (None,)
+        group_nk = self.group.natural_key() \
+            if self.group else (None, None, None)
+
+        return (self.begin_date, self.end_date) + rep_nk + group_nk
 
     class Meta:
         ordering = ('-end_date',)
